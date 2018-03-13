9b79446a20764528bc7c99ee66505a520fa19250
Phillip Vorce <vorcepa@gmail.com>
2018-03-13 13:55:38 -0500

    Controller now reads sensor -> checks error -> runs a (currently incorrect) kp -> modifies gain -> sends gain to change the direction of motion

--- Controller.py
+++ Controller.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import math
+import utils
 pg.init()
 
 
@@ -11,8 +12,11 @@ class Controller():
     """
     def __init__(self):
         self.radian = 0
-        self.omega = .015  # FOR PROPER PID THIS NEEDS TO BE CHANGED TO GAIN
+        self.omega = 0
+        self.omegaMin = -1000
+        self.omegaMax = 1000
 
+        self.errorCurrent = 0
     """
     testing the logic for control.  To add:
         kp (proportional)
@@ -20,29 +24,43 @@ class Controller():
         kd (derivative)
     Not sure if these should be individual methods.
     """
-    def changeDir(self, activeKey, feedback):
+    def changeDir(self, gain):
+        if gain is not 0:
+            print(gain)
+            self.omega += gain
+            self.radian += self.omega
+            if self.radian > 2:
+                self.radian = 0
+            elif self.radian < 0:
+                self.radian = 2
+        else:
+            self.omega = 0
+        if self.omega > self.omegaMax:
+            self.omega = self.omegaMax
+        elif self.omega < self.omegaMin:
+            self.omega = self.omegaMin
+        # activeKey is only here because keypresses can still control the car
+        activeKey = pg.key.get_pressed()
         if activeKey[pg.K_RIGHT] and not activeKey[pg.K_LEFT]:
+            self.omega = .015
             self.radian += self.omega
             if self.radian > 2:
                 self.radian = 0
         elif activeKey[pg.K_LEFT] and not activeKey[pg.K_RIGHT]:
-            self.radian -= self.omega
-            if self.radian < 0:
-                self.radian = 2
-        if feedback == 0:
+            self.omega = -.015
             self.radian += self.omega
-            if self.radian > 2:
-                self.radian = 0
-        elif feedback == 1:
-            self.radian -= self.omega
             if self.radian < 0:
                 self.radian = 2
+        else:
+            self.omega = 0
+            self.radian += self.omega
+        
 
         cos_theta = math.cos(self.radian*math.pi)
         sin_theta = math.sin(self.radian*math.pi)
         return (cos_theta, sin_theta, self.radian)
 
-    def readSensors(self, sensorTest):
+    def readSensors(self, sensorList, sensorOffsets, sensorTest):
         """
         Still building.  Eventually the process needs to be:
         readSensor -> PID control modifies gain based on readSensor ->
@@ -51,19 +69,36 @@ class Controller():
         if sensorTest is not None:
             feedback = [None] * len(sensorTest)
             for i in range(len(sensorTest)):
-                if sensorTest[i] == (0, 0, 0):
-                    feedback[i] = 0
-                elif sensorTest[i] == (128, 128, 128):
-                    feedback[i] = 1
-                else:
-                    feedback[i] = 2
+                if sensorTest[i] is not None:
+                    if sensorTest[i][0] == (utils.GREY):
+                        feedback[i] = 0
+                    elif sensorTest[i][0] == (utils.GOLD):
+                        feedback[i] = 1
+                    else:
+                        feedback[i] = 2
         else:
             feedback = [2]
-        # activeKey is only here because keypresses can still control the car
-        activeKey = pg.key.get_pressed()
 
-        output = self.changeDir(activeKey, feedback)
+        output = self.PID(sensorList, sensorOffsets, feedback)
         return output
 
-    def PID(self):
-        pass
+    def PID(self, sensorList, sensorOffsets, feedback):
+        error = 0
+        for i in range(len(feedback)):
+            if sensorOffsets[i] < 1:
+                if feedback[i] is None:
+                    error += -3
+                else:
+                    feedback[i] = -feedback[i]
+                    error += feedback[i]
+            else:
+                if feedback[i] is None:
+                    error += 3
+                else:
+                    error += feedback[i]
+        kp = .008*error
+        if -.005 < kp < .005:
+            kp = 0
+#        print(kp)
+        output = self.changeDir(kp)
+        return output
