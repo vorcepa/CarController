3b884c9ef89441ee3b6157ffd9d6d006ce21b3b9
Phillip Vorce <vorcepa@gmail.com>
2018-03-13 14:02:07 -0500

    Changed sensors for when they are drawn to an empty square rather than a filled one.  Sensors now function when made visible.  Shortened variable names.  Updated docstrings to reflect changes

--- Sensors.py
+++ Sensors.py
@@ -6,16 +6,15 @@ pg.init()
 
 class Sensor(pg.sprite.Sprite):
     """
-    Pass in variables fillToggle, radianOffset, distance.
+    Pass in variables display, rOffset, dOffset, and weight.
 
-    fillToggle is used for physically looking at the sensor's location.
+    display is used for physically looking at the sensor's location.
     When instantiating the sensor class, set fillToggle to False to hide
     the sensor (normal functionality).  Set fillToggle to True to show
-    the sensor.  Not that the sensor will read the image.fill color rather
-    than what is 'underneath' it on the game window surface.  This breaks
-    the functionality of the sensor, but is useful for testing.
+    the sensor.  The sensor will behave as normal, it will just be
+    visible to the user
 
-    radianOffset is the radial (angular) offset from the direction of motion
+    rOffset is the radial (angular) offset from the direction of motion
     of the car.  If you want a sensor that is in-line with the direction of
     motion, set this variable to 0. If you want the sensor to be opposite
     the direction of motion (ie, behind the car), set this number to 1.  Use
@@ -25,65 +24,82 @@ class Sensor(pg.sprite.Sprite):
     *** NOTE THAT POSITIVE ROTATION IS CURRENTLY CLOCKWISE.  THIS WAS A DUMB
     MISTAKE ON MY PART. ***
 
-    distanceOffset is the linear distance from the center of the car, in
+    dOffset is the linear distance from the center of the car, in
     pixels. It is recommended that this number be greater than 0, with a range
     of ~25 pixels to the edge of the screen (<300 pixels by default).
+    
+    weight is how much the sensor affects the gain.  It is recommended that
+    sensors close to the origin of the car be given a higher weight.
 
     production example:
-        self.sensorFrontCenter = Sensor(False, 0, 125)
+        self.sensorFrontCenter = Sensor(False, 0, 125, [SOMETHING])
 
         - Sensor will be invisible and looking at pixels directly
         in-line with the direction of motion, and in front of the
         car
 
     testing example:
-        self.sensorRightSide = Sensor(True, .5, 100)
+        self.sensorRightSide = Sensor(True, .5, 100, [SOMETHING])
 
-        - Sensor will be visible (and nonfunctional), and
+        - Sensor will be visible, and
         will be located at an angle of 90 ° clockwise from
-        the direction of motion
+        the direction of motion.
+
+        3/12/18
+        TO DO: Determine weights to describe what a number does
+        (example: weight = 5 is large weight, .01 is small weight,
+        or 2 is small weight, 20 is large weight, etc.)
     """
-    def __init__(self, fillToggle, radianOffset, distanceOffset):
+    def __init__(self, gameWindow, display, rOffset, dOffset, weight):
         pg.sprite.Sprite.__init__(self)
 
-        self.image = pg.Surface((25, 25))
-        self.rect = pg.Surface.get_rect(self.image)
-        self.fillToggle = fillToggle
-        if not isinstance(self.fillToggle, bool):
+        self.gameWindow = gameWindow
+        self.color = utils.WHITE
+        self.x = 24
+        self.y = 24
+        self.centerx = self.x // 2
+        self.centery = self.y // 2
+        self.area = (self.x, self.y)
+#        self.image = pg.Surface((25, 25))
+#        self.rect = pg.Surface.get_rect(self.image)
+#        self.rectDraw = pg.draw.rect(self.image, utils.WHITE, self.rect, 2)
+        self.display = display
+        if not isinstance(self.display, bool):
             pg.quit()
-            raise TypeError("Expected variable of type boolean, got type {}".format(type(self.fillToggle)))
-        self.image.fill(utils.GREEN)
+            raise TypeError("Expected variable of type boolean, got type {}".format(type(self.display)))
 
-        self.radianOffset = radianOffset
-        if type(self.radianOffset) not in (int, float):
+        self.rOffset = rOffset
+        if type(self.rOffset) not in (int, float):
             pg.quit()
-            raise TypeError("Expected variable of type int or float, got type {}".format(type(self.radianOffset)))
-        self.distanceOffset = distanceOffset
-        if type(self.distanceOffset) not in (int, float):
+            raise TypeError("Expected variable of type int or float, got type {}".format(type(self.rOffset)))
+        self.dOffset = dOffset
+        if type(self.dOffset) not in (int, float):
             pg.quit()
-            raise TypeError("Expected variable of type int or float, got type {}".format(type(self.radianOffset)))            
+            raise TypeError("Expected variable of type int or float, got type {}".format(type(self.dOffset)))            
 
         self.testCD = 30
         self.testCDMax = 30
 
     def move(self, directionLoc, radian):
         self.testCD -= 1
-        radian += self.radianOffset
-        x = int(round(self.distanceOffset*math.cos(radian*math.pi), 0))
-        y = int(round(self.distanceOffset*math.sin(radian*math.pi), 0))
-        self.rect.x = directionLoc[0] + x
-        self.rect.y = directionLoc[1] + y
+        radian += self.rOffset
+        x = int(round(self.dOffset*math.cos(radian*math.pi), 0))
+        y = int(round(self.dOffset*math.sin(radian*math.pi), 0))
+        self.x = directionLoc[0] + x
+        self.y = directionLoc[1] + y
+        self.centerx = self.x + 12
+        self.centery = self.y + 12
 #        if self.testCD <= 0:
 #            self.testCD = self.testCDMax
 #            print(x, self.rect.x)
 
     def update(self, gameWindow):
-        if self.fillToggle:
-            gameWindow.blit(self.image, (self.rect.x, self.rect.y))
+        if self.display:
+            self.image = pg.draw.rect(gameWindow, utils.WHITE, (self.x, self.y, 24, 24), 1)
 
         try:
-            getColor = gameWindow.get_at((self.rect.centerx,
-                                          self.rect.centery))
-            return getColor[:3]
+            getColor = gameWindow.get_at((self.centerx,
+                                          self.centery))
+            return [getColor[:3]]
         except IndexError:
             return
